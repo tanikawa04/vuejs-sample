8f94c7972791705b9576f9f3e304ef68fe75e9e9
tanikawa04 <tanikawa04@gmail.com>
2017-12-10 03:08:56 +0900

    カウンターアプリ

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p class="md-display-1">Clicked: {{ $store.state.count }} times, count is {{ evenOrOdd }}.</p>
+    <md-button class="md-raised md-primary" @click="increment">+</md-button>
+    <md-button class="md-raised md-accent" @click="decrement">-</md-button>
+    <md-button class="md-raised" @click="incrementIfOdd">Increment if odd</md-button>
+    <md-button class="md-raised" @click="incrementAsync">Increment async</md-button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters([
+      'evenOrOdd'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'increment',
+      'decrement',
+      'incrementIfOdd',
+      'incrementAsync'
+    ])
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Counter from '@/components/Counter'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Counter',
+      component: Counter
     }
   ]
 })
